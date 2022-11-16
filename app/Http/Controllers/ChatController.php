a1fa04f64fd735ba698e449655390ce379e2e0e7
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-11-16 15:53:14 +0800

    Create Laravel Project

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    public function send(Request $req)
+    {
+        return [];
+    }
+}
