f506d45675c3ec5252e632fd3f3a9d1f493ada4e
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-11-19 00:46:26 +0800

    create chat broadcast

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\Chat;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
     public function send(Request $req)
     {
+        event(new Chat($req->user, $req->message));
         return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::name('chat')->group(function () {
-    Route::post('/send', [ChatController::class, 'send']);
+Route::prefix('chat')->group(function () {
+    Route::post('send', [ChatController::class, 'send']);
 });
